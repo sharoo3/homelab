847ae894fd8b8555494a7d7adda19aa00c2a4328
eirayimiguk <eirayimiguk@gmail.com>
2024-05-04 18:17:25 +0900

    feat: add pi-hole container

---
+++ terraform/proxmox/container/pi-hole/container.tf
@@ -0,0 +1,26 @@
+resource "proxmox_lxc" "pi-hole" {
+  target_node     = "minase"
+  vmid            = 100
+  hostname        = "pi-hole"
+  ostemplate      = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
+  unprivileged    = true
+  pool            = "terraform"
+  cores           = 1
+  memory          = 1024
+  swap            = 1024
+  onboot          = true
+  start           = true
+  ssh_public_keys = file("${path.module}/key.pub")
+  tags            = "debian;management"
+
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "192.168.0.101/24"
+  }
+
+  rootfs {
+    storage = "local-lvm"
+    size    = "8G"
+  }
+}
