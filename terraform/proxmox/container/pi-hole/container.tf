81763e44563a12b116350c995357bf503adf0612
eirayimiguk <eirayimiguk@gmail.com>
2024-05-05 15:00:16 +0900

    feat: change ip

--- terraform/proxmox/container/pi-hole/container.tf
+++ terraform/proxmox/container/pi-hole/container.tf
@@ -1,23 +1,26 @@
 resource "proxmox_lxc" "pi-hole" {
-  target_node     = "minase"
-  vmid            = 100
-  hostname        = "pi-hole"
-  ostemplate      = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
-  unprivileged    = true
-  pool            = "terraform"
-  cores           = 1
-  memory          = 1024
-  swap            = 1024
-  onboot          = true
-  start           = true
-  ssh_public_keys = file("../../../../resources/public_keys/key.pub")
-  tags            = "debian;management"
+  target_node  = "minase"
+  vmid         = 100
+  hostname     = "pi-hole"
+  ostemplate   = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
+  unprivileged = true
+  pool         = "terraform"
+  cores        = 1
+  memory       = 1024
+  swap         = 1024
+  onboot       = true
+  start        = true
+  tags         = "debian;management"
+
+  ssh_public_keys = <<EOT
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO/t8WxckiAay1Ql5kyoyxlIDlXU0M17Epu5zSqDWjVo
+  EOT
 
   network {
     name   = "eth0"
     bridge = "vmbr0"
-    ip     = "192.168.0.101/24"
-    gw     = "192.168.0.100"
+    ip     = "192.168.100.3/24"
+    gw     = "192.168.100.100"
   }
 
   rootfs {
