ed6f93a18a1c03c5a723f103538f7941cc107faf
sharoo3 <nakajima.k0301@gmail.com>
2025-10-24 00:22:34 +0900

    feat: add initial configuration for image-storage container

---
+++ terraform/proxmox/container/image-storage/container.tf
@@ -0,0 +1,30 @@
+resource "proxmox_lxc" "container" {
+  target_node  = "minase"
+  vmid         = 100
+  hostname     = "image-storage"
+  ostemplate   = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
+  unprivileged = true
+  pool         = "terraform"
+  cores        = 1
+  memory       = 1024
+  swap         = 1024
+  onboot       = true
+  start        = true
+  tags         = "service;ubuntu"
+
+  ssh_public_keys = <<-EOT
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEqLIvRqHO6fNvuErP/DnBGZEGZlcZ4OBv2HXkodJ93z
+  EOT
+
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "192.168.0.100/24"
+    gw     = "192.168.0.1"
+  }
+
+  rootfs {
+    storage = "local-lvm"
+    size    = "30G"
+  }
+}
